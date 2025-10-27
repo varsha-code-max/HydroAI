df581631c5a057a362d29e79eb13a28104dea650
varsha-code-max <prajasurip@gmail.com>
2025-10-27 21:23:34 +0530

    Add files via upload

---
+++ test_pickle_load.py
@@ -0,0 +1,7 @@
+import joblib
+
+scaler = joblib.load("C:/Users/Sri Varsha/Desktop/edunet/scaler.pkl")
+print("✅ Scaler loaded successfully!")
+
+model = joblib.load("C:/Users/Sri Varsha/Desktop/edunet/hydro_model.pkl")
+print("✅ Model loaded successfully!")
